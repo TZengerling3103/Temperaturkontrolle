b8e470eb3413b03593afe05edcffe6385f4123e4
TZengerling3103 <115074878+TZengerling3103@users.noreply.github.com>
2022-10-05 11:48:05 +0200

    Add Files

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="icon" href="/578113.png" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temperaturkontrolle</title>
+</head>
+<body>
+    <nav>
+            <div class="row">
+                <div class="col-sm-4">
+                    <img class="logo" src="/image.png" alt="Logo">
+                </div>
+                <div class="col-sm-6"></div>
+                <div class="col-sm-2">
+                    <button class="btn sign-in btn-primary" type="submit">Anmelden</button>
+                </div>
+            </div>        
+        
+    </nav>
+    <main>
+        <div class="content">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Sensor</th>
+                    <th scope="col">Temperatur</th>
+                    <th scope="col">Zustand</th>
+                  </tr>
+                </thead>
+                <br>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                </tbody>
+            </table>
+        </div>
+    </frameset>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
